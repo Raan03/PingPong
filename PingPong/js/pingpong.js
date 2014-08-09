4018852aae076e9f1493271b6aaef300acf44b5d
R. Annaert <Raan03@gmaiL.com>
2014-08-09 04:50:20 +0200

    Initial Commit

---
+++ PingPong/js/pingpong.js
@@ -0,0 +1,41 @@
+﻿var KEY =
+    {
+        UP: 38,
+        DOWN: 40,
+        W: 90,
+        S: 83
+    }
+
+$(function () {
+    // listen to keydown
+    $(document).keydown(function (e) {
+        console.log(e.which);
+        switch (e.which) {
+            case KEY.UP: //arrow up
+                // get paddleB Y value (int)
+                var top = parseInt($("#paddleB").css("top"));
+                // move it 20 pixels
+                $("#paddleB").css("top", top - 20);
+                break;
+            case KEY.DOWN: //arrow down
+                //get paddleB Y value (int)
+                var top = parseInt($("#paddleB").css("top"));
+                // move it 20 pixels
+                $("#paddleB").css("top", top + 20);
+                break;
+            case KEY.W:
+                //get paddleA Y value (int)
+                var top = parseInt($("#paddleA").css("top"));
+                // move it 20 pixels
+                $("#paddleA").css("top", top - 20);
+                break;
+            case KEY.S:
+                //get paddleA Y value (int)
+                var top = parseInt($("#paddleA").css("top"));
+                // move it 20 pixels
+                $("#paddleA").css("top", top + 20);
+                break;
+
+        }
+    });
+});
